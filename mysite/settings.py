5f196b33e41e6ca8b82ed4b3c9af59af52bd572b
quevedo79 <quevedo79@gmail.com>
2016-03-15 17:15:18 +0000

    change to use SQLite and customize admin pages

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,23 +75,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'testDjangoApp',
-        'USER': 'djangoTestUser',
-        'PASSWORD': 'dj1angoT3st',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'testDjangoApp',
+#        'USER': 'djangoTestUser',
+#        'PASSWORD': 'dj1angoT3st',
+#        'HOST': 'localhost',
+#        'PORT': '3306',
+#    }
+#}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
